0eb70c9085565d41a00234b3a252488ed96b9e0d
Aaron Mavrou <amavrou@uwo.ca>
2018-11-11 23:30:08 +0000

    modified a lot

--- public/index.js
+++ public/index.js
@@ -1,60 +1,80 @@
+var myArray = [];
 
+function encodeHTML(s) {
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+}
 
 function hola(){
     const url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/create';
-// The data we are going to send in our request
-var itemName = document.getElementById("productName").value;
-var itemPrice = document.getElementById("productPrice").value;
-var itemQuantity = document.getElementById("productQuantity").value;
-var itemTax = document.getElementById("productTax").value;
-let data = {
-    name: itemName,
-    price: itemPrice,
-    taxRate: itemQuantity,
-    quantity: itemTax
+    // The data we are going to send in our request
+    var itemName = document.getElementById("productName").value;
+    var itemPrice = document.getElementById("productPrice").value;
+    var itemQuantity = document.getElementById("productQuantity").value;
+    var itemTax = document.getElementById("productTax").value;
+    
+    alert(itemQuantity);
+    
+    if (itemName.length == 0 || itemPrice.length == 0){
+        alert("Your item name or price cannot be empty. Please try again");
+    }
+    
+    else{
+        let data = {
+            name: encodeHTML(itemName),
+            price: encodeHTML(itemPrice),
+            taxRate: encodeHTML(itemQuantity),
+            quantity: encodeHTML(itemTax)
+            }
+        // The parameters we are gonna pass to the fetch function
+        let fetchData = { 
+            method: 'POST', 
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }
+
+        fetch(url, fetchData)
+        .then(function() {
+            alert("you maybe did it!");
+        }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error', error));
+
+        getAll();
+    }
 }
 
-// The parameters we are gonna pass to the fetch function
-let fetchData = { 
-    method: 'POST', 
-    body: JSON.stringify(data),
-    headers: new Headers({
-        'Content-Type': 'application/json'
-    })
-}
-
-fetch(url, fetchData)
-.then(function() {
-    alert("you maybe did it!");
-}).then(res => res.json())
-.then(response => console.log('Success:', JSON.stringify(response)))
-.catch(error => console.error('Error', error));
-
-getAll();
-}
-
-var myArray = [];
-var j=1000;
-var k = -1;
-var r= 100000;
-var m = -100000;
 function getAll(){
     removeAll();
     const url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/getall';
     fetch(url)
     .then((resp)=> resp.json())
     .then(function(data){
+        var startRow = 1;
+        var myTable = '<table id = "myTableId"><tr><th>Product Name</th><th>Price</th><th>Quantity</th><th>Tax Rate </th></tr>';
         for(i = 0; i< data.length; i++){
-           createElement("LI",(i).toString(), data[i].name + "," + data[i].price + "," + data[i].taxRate + "," + data[i].quantity);
-           createTextField("input", (j++).toString());
-           createButton("button", (r++).toString(), "Change Quantity");
-           var addBreak = document.createElement("br");
-           document.body.appendChild(addBreak);
-           createTextField("input", (k--).toString());
-           createButton("button", (m--).toString(), "Change Tax Rate");
-           myArray[i]= data[i]._id;
-           console.log(data[i].name);
+             var thebuttonid = "button" + i;
+             var e = document.createTextNode(data[i].name);
+             var f = document.createTextNode(data[i].price);
+             var g = document.createTextNode(data[i].quantity);
+             var h = document.createTextNode(data[i].taxRate);
+             var ee = document.createElement("div");
+             var ff = document.createElement("div");
+             var gg = document.createElement("div");
+             var hh = document.createElement("div");
+             ee.appendChild(e);
+             ff.appendChild(f);
+             gg.appendChild(g);
+             hh.appendChild(h);
+           myTable += "<tr><td>" + ee.innerHTML + "</td><td>" + ff.innerHTML + "</td><td>" + gg.innerHTML + "</td><td>" + hh.innerHTML +  
+        "</td><td><button onclick='deleteProduct(" + i + ")'>Delete</button></td><td>" + "</td><td><input type='number' min = "+ 0 +" name='amount'></td><td>" +
+        "</td><td><button onclick='quantityUpdate("+ i + ',' + startRow +")'>Update Quantity</button></td><td>" + "</td><td><input type='number' min = "+ 0 +" name='amount'></td><td>" +
+        "</td><td><button onclick='taxRateUpdate(" + i + ',' + startRow + ")'>Update Tax (%)</button></td>"
+        myArray[i]= data[i]._id;
+        startRow = startRow+1;
         }
+        document.getElementById("basket").innerHTML = myTable;
     })
 }
 
@@ -69,39 +89,18 @@ function removeAll(){
     })
 }
 
-function createTextField(el, elId){
-    var c = document.createElement(el);
-    c.setAttribute('id', elId);
-    document.body.appendChild(c);
-}
-
-function createButton(elem, elemId, theText){
-    var d = document.createElement(elem);
-    var e = document.createTextNode(theText);
-    d.setAttribute('id', elemId);
-    d.appendChild(e);
-    document.body.appendChild(d);
-    
-}
-
-function createElement(element, elementId, itemDetails){
-   var a = document.createElement(element);
-   var b = document.createTextNode(itemDetails);
-   a.setAttribute('id', elementId);
-   a.appendChild(b);
-   document.body.appendChild(a);
-}
-
 function removeElement(theid){
-    var element = document.getElementById(theid)
+    var element = document.getElementById(theid);
     element.parentElement.removeChild(element);
 }
 
-function quantityUpdate(updateItemId, newQuantity){
-    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/updateQuantity/' + updateItemId;
+function quantityUpdate(updateItemId, theRow){
+    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/updateQuantity/' + myArray[updateItemId];
+    var theTable = document.getElementById("myTableId");
+    var theNewValue = theTable.rows[theRow].cells[6].children[0].value;
     
     var data = {
-        quantity: newQuantity
+        quantity: encodeHTML(theNewValue)
     }
     
     let fetchData = { 
@@ -118,13 +117,17 @@ function quantityUpdate(updateItemId, newQuantity){
     }).then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error', error));
+    
+    getAll();
 }
 
-function taxRateUpdate(updateTaxRateId, newTaxRate){
-    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/updateTaxRate/' + updateTaxRateId;
+function taxRateUpdate(updateTaxRateId, myRowNum){
+    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/updateTaxRate/' + myArray[updateTaxRateId];
+    var theTable = document.getElementById("myTableId");
+    var theNewTaxRate = theTable.rows[myRowNum].cells[10].children[0].value;
     
     var data = {
-        taxRate: newTaxRate
+        taxRate: encodeHTML(theNewTaxRate)
     }
     
     let fetchData = { 
@@ -141,10 +144,12 @@ function taxRateUpdate(updateTaxRateId, newTaxRate){
     }).then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error', error));
+    
+    getAll();
 }
 
 function deleteProduct(deleteItemIdId){
-    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/delete/' + deleteItemId;    
+    var url = 'https://se3316-amavrou-lab3-amavrou.c9users.io/products/delete/' + myArray[deleteItemIdId];    
 
     let fetchData = { 
         method: 'DELETE', 
@@ -159,4 +164,5 @@ function deleteProduct(deleteItemIdId){
     }).then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error', error));
+    getAll();
 }
